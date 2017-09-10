2ecc4ed928ecd8893c671373354e269865dea89c
Ramil Rakhmatullin <rep555333@gmail.com>
2017-09-11 01:02:36 +0300

    added slick slider

---
+++ app/js/slick-settings.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    var slider = $('.promo-slider__slider');
+
+    slider.slick({
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: false
+    });
+});
