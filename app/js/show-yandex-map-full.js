cef2f46b6f0b1e8e8c6bcfac77fc88d1af04460b
Roman Iov <mickeyeasy@gmail.com>
2017-11-30 10:52:31 +0800

    aga

--- app/js/show-yandex-map-full.js
+++ app/js/show-yandex-map-full.js
@@ -1,10 +1,31 @@
 var showyandexmap = $('#yandexmap-show');
+var CloseYandexMap = $('#yandexmap-close');
 
 if (showyandexmap) {
   showyandexmap.on('click', showYandexMap);
 }
 
+if (CloseYandexMap) {
+  CloseYandexMap.on('click', closeYandexMap);
+}
+
 function showYandexMap(event) {
   event.preventDefault();
   $('#yandexmap').css("z-index","2000 ");
+  $('#yandexmap-close').addClass("modal-close--yandex-map-show");
+  
+  if($('*').is($('#promo-picture-man'))){
+    $('#promo-picture-man').hide();
+  }
 }
+
+function closeYandexMap(event) {
+  event.preventDefault();
+  $('#yandexmap-close').removeClass("modal-close--yandex-map-show");
+  $('#yandexmap').css("z-index","-1 ");
+  
+  if($('*').is($('#promo-picture-man'))){
+    $('#promo-picture-man').show();
+  }
+}
+
